7ae40947bf95f21aa6371b3fbf2489eebe4a619c
abhisekchand8155 <chandabhisek1234@gmail.com>
2024-09-23 18:44:15 +0000

    varriables

--- basic-js/datatypes.js
+++ basic-js/datatypes.js
@@ -1,20 +1,45 @@
-// let age=16
+
+// let age = 34;
 // console.log(age) 
 // console.log(typeof(age)) 
-// let y=Symbol("hello")
-// console.log(y.description) 
+
+// var a = "web bocket";
+// console.log(a)
+// console.log(typeof(a))
+
+// let x = BigInt("678");
+// console.log(x)
+// console.log(typeof(x))
+
+// const a = null
+// console.log(a)
+// console.log(typeof(a))
+
+// let mySalary;
+// console.log(mySalary)
+// console.log(typeof(mySalary))
+
+// let y = Symbol("hello");
+// console.log(y)
+// console.log(y.description)
 // console.log(typeof(y))
-// let arr=[50,60,70,80]
-// console.log(arr [2]) //70
-// console.log(typeof(arr))//object
+// symbol gives us a unique property so that we used for javascript
+
+// let arr = [98,76,45,89,34,90,32,67]
+// // console.log(arr)
+// console.log(arr[2]) // 45
+// console.log(arr[6]) // 32
+// console.log(typeof(arr)) // object
+
+
 let sidharth = {
-    "name"="sidharth pani"
-    "place"="usa"
-    "age"="45"
-    "salary"=4552200
+    "name" : "sidharth pani",
+    "place" : "new york",
+    "age" : 67,
+    "salary" : 56789
 }
+
 console.log(sidharth)
-console.log(sidharth[place])
-console.log(sidharth[age])
-console.log(sidharth[salary])
 console.log(typeof(sidharth))
+console.log(sidharth["age"])
+console.log(sidharth["salary"])
